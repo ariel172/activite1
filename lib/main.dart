0f6abea236473eaed18749504095db8ba98babc0
Ariel Yamien <arielyamien770@gmail.com>
2026-01-11 07:21:59 +0000

    class de la partie rubrique et correction de quelques éléments dans les autres class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,10 +8,10 @@ class MonAppli extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Magazine",
       debugShowCheckedModeBanner: false,
-      home: partieIcone(),
+      home: PageAccueil(),
     );
   }
 }
@@ -45,48 +44,55 @@ class PageAccueil extends StatelessWidget {
           onPressed: () {},
         ),
       ),
-    body: Center(
-        child: Image.asset('assets/images/magazineInfo.jpg',
-          fit: BoxFit.cover
-        ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-              content: Text("Tu as cliqué dessus"),
-          )
-        );
-      },
-      backgroundColor: Colors.pink,
-      child: const Text(
-        "Click",
-        style: TextStyle(color: Colors.white),
+    body:SingleChildScrollView(
+      child: const Column(
+        children: [
+          Image(image: AssetImage("assets/images/magazineInfo.jpg")
+          ),
+          PartieTitre(),
+          PartieTexe(),
+          PartieIcone(),
+          PartieRubrique()
+        ],
       ),
-    ),
+    )
+   // floatingActionButton: FloatingActionButton(
+      // onPressed: (){
+     //   ScaffoldMessenger.of(context).showSnackBar(
+     //     const SnackBar(
+    //          content: Text("Tu as cliqué dessus"),
+     //     )
+    //    );
+    //  },
+     // backgroundColor: Colors.pink,
+    //  child: const Text(
+    //    "Click",
+      //  style: TextStyle(color: Colors.white),
+     // ),
+    //),
     );
   }
 }
 
-class partieTitre extends StatelessWidget {
-  const partieTitre({super.key});
+class PartieTitre extends StatelessWidget {
+  const PartieTitre({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.all(20),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text("Bienvenue au Magazine Infos",
           style: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            fontWeight: FontWeight.w800,
           ),
           ),
           Text("Votre magazine numérique, votre source d'inspiration",
-            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
           )
         ],
       ),
@@ -94,20 +100,23 @@ class partieTitre extends StatelessWidget {
   }
 }
 
-class partieTexe extends StatelessWidget {
-  const partieTexe({super.key});
+class PartieTexe extends StatelessWidget {
+  const PartieTexe({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: const Text("Magazine Infos est bien plus simple magazine d'information"),
+      child: const Text(
+          "Magazine Infos est bien plus simple magazine d'information."
+              "C'est votre passerelle vers le monde"
+      ),
     );
   }
 }
 
-class partieIcone extends StatelessWidget {
-  const partieIcone({super.key});
+class PartieIcone extends StatelessWidget {
+  const PartieIcone({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -121,7 +130,7 @@ class partieIcone extends StatelessWidget {
             child: Column(
               children: [
                 Icon(Icons.phone, color: Colors.pink),
-                SizedBox(height: 6,),
+                SizedBox(height: 5,),
                 Text("Tel".toUpperCase(),
                   style: TextStyle(color: Colors.pink),
                 )
@@ -133,7 +142,7 @@ class partieIcone extends StatelessWidget {
             child: Column(
               children: [
                 Icon(Icons.email, color: Colors.pink),
-                SizedBox(height: 6,),
+                SizedBox(height: 5,),
                 Text("mail".toUpperCase(),
                   style: TextStyle(color: Colors.pink),
                 )
@@ -145,12 +154,35 @@ class partieIcone extends StatelessWidget {
             child: Column(
               children: [
                 Icon(Icons.share, color: Colors.pink),
-                SizedBox(height: 6,),
+                SizedBox(height: 5,),
                 Text("partage".toUpperCase(),
                   style: TextStyle(color: Colors.pink),
                 )
               ],
             )
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PartieRubrique extends StatelessWidget {
+  const PartieRubrique({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.asset("assets/images/design.jpg"),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.asset("assets/images/presse.jpg"),
           )
         ],
       ),
